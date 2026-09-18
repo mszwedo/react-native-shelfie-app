4a71a02314779c9d5b8de2f6a6c38649dee2b295
Mike Szwedo <mike.szwedo@gmail.com>
2026-09-18 16:30:49 -0700

    Added a light and dark theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,29 @@
 import { Stack } from 'expo-router'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, useColorScheme } from 'react-native'
+import { Colors } from '../constants/Colors'
+import { StatusBar } from 'expo-status-bar'
 
 const RootLayout = () => {
-    return (
-        <Stack screenOptions={{
-            headerStyle: {
-                backgroundColor: '#ddd', // blue: #5977b4'
-            },
-            headerTintColor: '#333',
-            headerTitleAlign: 'center',
-        }}>
-            <Stack.Screen name="index" options={{ title: 'Home' }} />
-            <Stack.Screen name="about" options={{ title: 'About' }} />
-            <Stack.Screen name="contact" options={{ title: 'Contact', headerShown: false }} />
-        </Stack>
-    )
+
+  const colorScheme = useColorScheme()
+  const theme = Colors[colorScheme === 'dark' ? 'dark' : 'light'] ?? Colors.light
+
+  return (
+    <>
+      <StatusBar style="auto" />
+      <Stack screenOptions={{
+        headerStyle: {
+            backgroundColor: theme.navBackground, // blue: #5977b4'
+        },
+        headerTintColor: theme.title,
+        headerTitleAlign: 'center',
+      }}>
+        <Stack.Screen name="index" options={{ title: 'Home' }} />
+        <Stack.Screen name="about" options={{ title: 'About' }} />
+        <Stack.Screen name="contact" options={{ title: 'Contact', headerShown: false }} />
+      </Stack>
+    </>
+  )
 }
 
 export default RootLayout
