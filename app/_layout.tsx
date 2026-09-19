e601a352be1e2918ac237c13dc7b4ac2063f2213
Mike Szwedo <mike.szwedo@gmail.com>
2026-09-18 19:24:25 -0700

    Added tab navigation for profile, create, and books screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -20,6 +20,8 @@ const RootLayout = () => {
       }}>
         <Stack.Screen name="index" options={{ title: 'Home' }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
+        
         <Stack.Screen name="about" options={{ title: 'About' }} />
         <Stack.Screen name="contact" options={{ title: 'Contact', headerShown: false }} />
       </Stack>
--- app/index.tsx
+++ app/index.tsx
@@ -29,6 +29,9 @@ const Home = () => {
       <Link href="/register" style={styles.link}>
         <ThemedText>Register Page</ThemedText>
       </Link>
+      <Link href="/profile" style={styles.link}>
+        <ThemedText>Profile Page</ThemedText>
+      </Link>
     </ThemedView>
   )
 }
