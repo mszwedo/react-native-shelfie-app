e88ce1f7311754ed120167ff2dbf33e2b61b5a58
Mike Szwedo <mike.szwedo@gmail.com>
2026-09-18 18:54:04 -0700

    Added a themed buttons and added Login and Register buttons

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,11 +1,17 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Pressable, Text } from 'react-native';
 import { Link } from 'expo-router'
 import ThemedView from '../../components/ThemedView'
 import ThemedText from '../../components/ThemedText'
 import Spacer from '../../components/Spacer';
+import ThemedButton from '../../components/ThemedButton';
 
 
 const Login = () => {
+
+  const handleSubmit = () => {
+    console.log('Login button pressed');
+  }
+
   return (
     <ThemedView style={styles.container}>
 
@@ -13,6 +19,10 @@ const Login = () => {
       <ThemedText title={true} style={styles.title}>
         Login to Your Account
       </ThemedText>
+
+      <ThemedButton onPress={handleSubmit}>
+        <Text style={{ color: '#f2f2f2'}}>Login</Text>
+      </ThemedButton>
 
       <Spacer height={100}/>
       <Link href="/register">
@@ -30,10 +40,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
   },
   title: {
     textAlign: 'center',
     fontSize: 18,
     marginBottom: 30,
-  }
+  },
 })
