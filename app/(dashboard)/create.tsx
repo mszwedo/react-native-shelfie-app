b453d164ca51cbeaf3282bc54f203eb90631ae74
Mike Szwedo <mike.szwedo@gmail.com>
2026-09-22 01:44:58 -0700

    Added the create book functionality

--- app/(dashboard)/create.tsx
+++ app/(dashboard)/create.tsx
@@ -1,19 +1,84 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Text, TouchableWithoutFeedback, Keyboard } from 'react-native'
+import { useBooks } from '../../hooks/useBooks'
+import { useRouter } from 'expo-router'
+import { useState } from 'react'
 
 import Spacer from "../../components/Spacer"
 import ThemedText from "../../components/ThemedText"
 import ThemedView from "../../components/ThemedView"
+import ThemedTextInput from '../../components/ThemedTextInput'
+import ThemedButton from '../../components/ThemedButton'
 
 const Create = () => {
+
+  const [title, setTitle] = useState("")
+  const [author, setAuthor] = useState("")
+  const [description, setDescription] = useState("")
+  const [loading, setLoading] = useState(false)
+
+  const { createBook } = useBooks()
+  const router = useRouter()
+
+  const handleSubmit = async () => {
+    if (!title.trim() || !author.trim() || !description.trim()) return
+
+    setLoading(true)
+
+    await createBook({ title, author, description })
+
+    // reset book fields
+    setTitle("")
+    setAuthor("")
+    setDescription("")
+
+    // redirect to books screen
+    router.replace('/books')
+
+    setLoading(false)
+  }
+
   return (
-    <ThemedView style={styles.container} safe>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <ThemedView style={styles.container} safe>
 
-      <ThemedText title={true} style={styles.heading}>
-        Add a New Book
-      </ThemedText>
-      <Spacer />
+        <ThemedText title={true} style={styles.heading}>
+          Add a New Book
+        </ThemedText>
+        <Spacer />
 
-    </ThemedView>
+        <ThemedTextInput 
+          style={styles.input}
+          placeholder="Book Title"
+          value={title}
+          onChangeText={setTitle}
+        />
+        <Spacer />
+
+        <ThemedTextInput 
+          style={styles.input}
+          placeholder="Author"
+          value={author}
+          onChangeText={setAuthor}
+        />
+        <Spacer />
+
+        <ThemedTextInput 
+          style={styles.multiline}
+          placeholder="Book Description"
+          value={description}
+          onChangeText={setDescription}
+          multiline={true}
+        />
+        <Spacer />
+
+        <ThemedButton onPress={handleSubmit} disabled={loading}>
+          <Text style={{ color: '#fff'}}>
+            {loading ? "Saving..." : "Create Book"}
+          </Text>
+        </ThemedButton>
+
+      </ThemedView>
+    </TouchableWithoutFeedback>
   )
 }
 
@@ -30,4 +95,17 @@ const styles = StyleSheet.create({
     fontSize: 18,
     textAlign: "center",
   },
+  input: {
+    padding: 20,
+    borderRadius: 6,
+    alignSelf: 'stretch',
+    marginHorizontal: 40,
+  },
+  multiline: {
+    padding: 20,
+    borderRadius: 6,
+    minHeight: 100,
+    alignSelf: 'stretch',
+    marginHorizontal: 40,
+  }
 })
--- context/BooksContext.tsx
+++ context/BooksContext.tsx
@@ -1,5 +1,7 @@
 import { createContext, useState } from 'react'
+import { useUser } from '../hooks/useUser'
 import { databases } from '../lib/appwrite'
+import { ID, Permission, Role } from 'react-native-appwrite'
 
 const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID
 const BOOKS_TABLE_ID = process.env.EXPO_PUBLIC_APPWRITE_BOOKS_TABLE_ID
@@ -7,7 +9,9 @@ const BOOKS_TABLE_ID = process.env.EXPO_PUBLIC_APPWRITE_BOOKS_TABLE_ID
 export const BooksContext = createContext({})
 
 export function BooksProvider({ children }) {
+
   const [books, setBooks] = useState([])
+  const { user } = useUser()
 
   async function fetchBooks() {
     try {
@@ -27,7 +31,17 @@ export function BooksProvider({ children }) {
 
   async function createBook(data) {
     try {
-
+      const newBook = await databases.createDocument(
+        DATABASE_ID, 
+        BOOKS_TABLE_ID, 
+        ID.unique(), 
+        {...data, userId: user.$id},
+        [
+          Permission.read(Role.user(user.$id)),
+          Permission.update(Role.user(user.$id)),
+          Permission.delete(Role.user(user.$id)),
+        ]
+      )
     } catch (error) {
       console.error(error.message)
     }
